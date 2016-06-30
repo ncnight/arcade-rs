12de4d2dcddb60eed23e637021385ef4a074f960
Nithin Chalapathi <nithin.ch10@gmail.com>
2016-06-29 21:37:55 -0600

    Finish 1.1 'A simple window'

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    //init sdl2
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    //window
+    let window = video.window("ArcadeRS Shooter", 800, 600).position_centered().opengl().build().unwrap();
+
+    let mut renderer = window.renderer().accelerated().build().unwrap();
+
+    //render
+    renderer.set_draw_color(Color::RGB(0,0,0));
+    renderer.clear();
+    renderer.present();
+
+    thread::sleep(Duration::from_millis(3000));
+
+}
